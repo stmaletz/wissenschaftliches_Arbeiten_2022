b67d7f0e882113637f26d33d7a93289e40c1f3f0
JacobGry <jacob.grytzka@tu-dortmund.de>
2022-11-29 19:34:33 +0100

    Update funfun.R

--- funfun.R
+++ funfun.R
@@ -2,14 +2,21 @@ require(beepr)
 
 funfun <- function(n = 61, max_width = 10, max_height = max_width,
                    max_xpos = 900, max_ypos = max_xpos){
-  
+  min_time <- 0.12
+  time_counter <- 2 * min_time
   for(i in seq_len(n)){
     x11(width = sample(max_width, 1),
         height = sample(max_height, 1),
         xpos = sample(max_xpos, 1),
         ypos = sample(max_ypos, 1))
-    beep(sample(11, 1))
-    Sys.sleep(rexp(1, 7))
+    time_step <- rexp(1, 7)
+    if(time_counter > min_time){
+      beep(sample(11, 1))
+      time_counter <- 0
+    } else{
+      time_counter <- time_counter + time_step
+    }
+    Sys.sleep(time_step)
   }
 }
 funfun()
