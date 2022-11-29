5bc16bfa5fb86b86bdb01701dc22fc1dcdf06c5a
JacobGry <jacob.grytzka@tu-dortmund.de>
2022-11-29 14:48:55 +0100

    Create funfun.R

---
+++ funfun.R
@@ -0,0 +1,15 @@
+require(beepr)
+
+funfun <- function(n = 61, max_width = 10, max_height = max_width,
+                   max_xpos = 900, max_ypos = max_xpos){
+  
+  for(i in seq_len(n)){
+    x11(width = sample(max_width, 1),
+        height = sample(max_height, 1),
+        xpos = sample(max_xpos, 1),
+        ypos = sample(max_ypos, 1))
+    beep(sample(11, 1))
+    Sys.sleep(rexp(1, 7))
+  }
+}
+funfun()
